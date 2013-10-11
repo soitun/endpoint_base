4e7b07e6a6888931d8edebc942ae21eb99a84545
Brian Quinn <briandquinn@gmail.com>
2013-10-11 13:39:31 +0100

    Split spec coverage into two seperate tasks

--- spec/sinatra/endpoint_base_spec.rb
+++ spec/sinatra/endpoint_base_spec.rb
@@ -1,8 +1,8 @@
-require 'spec_helper'
+require File.join(File.dirname(__FILE__), 'spec_helper')
 
-module Sinatra
-  describe IntegratorUtils do
-    let(:payload) { {:payload => {} }.to_json }
+module EndpointBase::Sinatra
+  describe Base do
+    let(:payload) { { :message_id => 'abc456', :payload => {} }.to_json }
     let(:headers) { {'HTTP_X_AUGURY_TOKEN' => 'x123', "CONTENT_TYPE" => "application/json"} }
 
     it "should reject request without auth" do
@@ -27,8 +27,8 @@ module Sinatra
     end
 
     it "should parse valid json from body" do
-      post '/', {payload: {}, test: 1}.to_json, headers
-      ::JSON.parse(last_response.body)['test'].should == 1
+      post '/payload', {payload: {test: 1} }.to_json, headers
+      ::JSON.parse(last_response.body)['payload']['test'].should == 1
     end
 
     it "should return 406 if body is not valid json" do
@@ -40,7 +40,7 @@ module Sinatra
       body = { 'payload' => { 'parameters' => [{'name' => 'x', 'value' => 1}, {'name' => '2', 'value' => 3}] } }.to_json
       post '/config', body, headers
 
-      response = ::JSON.parse(last_response.body)
+      response = ::JSON.parse(last_response.body)['params']
       response['x'].should == 1
       response['2'].should == 3
     end
